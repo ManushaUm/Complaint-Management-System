c3d8de04fed2fe3d489bab68c94b77f74573cf67
G.C.M. Perera <96817609+CharithManaujayaMUTEC@users.noreply.github.com>
2024-10-02 23:40:51 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -13,30 +13,6 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-<<<<<<< HEAD
-
-Route::get('/lodgenew', [NewComplaintController::class, 'lodgeNew'])->name('newcomplaint');
-
-Route::post('/complaints', [NewComplaintController::class, 'store'])->name('complaints.store');
-
-//Route::get('/viewcomplaints')->name('viewcomplaint');
-
-Route::get('/viewcomplaints', function () {
-    return view('viewcomplaint');
-})->middleware(['auth', 'verified'])->name('viewcomplaint');
-
-Route::get('/searchcomplaints', function () {
-    return view('searchcomplaints');
-})->middleware(['auth', 'verified'])->name('searchcomplaints');
-
-Route::get('/users', function () {
-    return view('useraccess');
-})->middleware(['auth', 'verified'])->name('users');
-
-
-=======
-<<<<<<< Updated upstream
-=======
 Route::get('/lodgenew', function () {
     return view('newcomplaint');
 })->middleware(['auth', 'verified'])->name('newcomplaint');
@@ -48,8 +24,6 @@ Route::get('/complaint', function () {
 Route::post('/complaintsave', [complaintcontroller::class , 'store'])->name('complaintstatus.store');
 
 
->>>>>>> Stashed changes
->>>>>>> 75fea66 (Update web.php)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
