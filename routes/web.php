75fea6657a7f562ec7e3c12e6b1f60e81443feb2
G.C.M. Perera <96817609+CharithManaujayaMUTEC@users.noreply.github.com>
2024-10-02 20:53:11 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\complaintcontroller;
 
 Route::get('/', function () {
     return view('auth.register');
@@ -11,6 +12,20 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+<<<<<<< Updated upstream
+=======
+Route::get('/lodgenew', function () {
+    return view('newcomplaint');
+})->middleware(['auth', 'verified'])->name('newcomplaint');
+
+Route::get('/complaint', function () {
+    return view('complaint');
+})->middleware(['auth', 'verified'])->name('complaint');
+
+Route::post('/complaintsave', [complaintcontroller::class , 'store'])->name('complaintstatus.store');
+
+
+>>>>>>> Stashed changes
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
