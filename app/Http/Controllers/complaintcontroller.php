b01e2fff281af60d59a48e9a977d4c5e5307ac2b
G.C.M. Perera <96817609+CharithManaujayaMUTEC@users.noreply.github.com>
2024-10-03 15:33:26 +0530

    Update complaintcontroller.php

--- app/Http/Controllers/complaintcontroller.php
+++ app/Http/Controllers/complaintcontroller.php
@@ -4,12 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\complaintstatus;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\File;
 
 class complaintcontroller extends Controller
 {
     public function store(Request $request)
     {
-        dd($request->input());
         $request->validate([
             'category' => 'required',
             'subcategory' => 'required',
@@ -21,19 +25,30 @@ class complaintcontroller extends Controller
             'description' => 'required',
         ]);
 
-        $complaint = new complaintstatus();
-        $complaint->category = $request->category;
-        $complaint->subcategory = $request->subcategory;
-        $complaint->location = $request->location;
-        $complaint->branch = $request->branch;
-        $complaint->resperson = $request->resperson;
-        $complaint->altperson = $request->altperson;
-        $complaint->pred = $request->pred;
-        $complaint->description = $request->description;
-        $complaint->save();
+        $data = array(
+            'category' => $request->category,
+            'subcategory' => $request->subcategory,
+            'location' => $request->location,
+            'branch' => $request->branch,
+            'resperson' => $request->resperson,
+            'altperson' => $request->altperson,
+            'pred' => $request->pred,
+            'description' => $request->description,
+        );
 
-        return redirect()->route('complaint')
-            ->with('success', 'Complaint created successfully.');
+        $id = DB::table('complaint_status')->insertGetId($data);
+
+        if ($id) {
+            return redirect()->back()->with('success', 'Complaint successfully logged');
+        } else {
+            return redirect()->back()->with('error', 'Error logged');
+        }
     }
+
+    public function typeview(){
+        $category = DB::table('complaint_type')->get();
+        return $category;
+    }
+
     
 }
