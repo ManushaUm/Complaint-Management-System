eba832b0778f338919b7b35f468809a09f4d20d5
G.C.M. Perera <96817609+CharithManaujayaMUTEC@users.noreply.github.com>
2024-10-03 15:33:20 +0530

    Update complaint.blade.php

--- resources/views/complaint.blade.php
+++ resources/views/complaint.blade.php
@@ -48,7 +48,7 @@
                                         <div class="mb-3 row">
                                             <label class="col-md-2 col-form-label">Category</label>
                                             <div class="col-md-3">
-                                                <select class="form-select" id='category'>
+                                                <select class="form-select" id='category' name='category'>
                                                     <option>Motor Claims</option>
                                                     <option>Non Motor Claims</option>
                                                     <option>Policy Serving</option>
@@ -63,7 +63,7 @@
                                         <div class="mb-3 row">
                                             <label class="col-md-2 col-form-label">Sub Category</label>
                                             <div class="col-md-3">
-                                                <select class="form-select" id='subcategory'>
+                                                <select class="form-select" id='subcategory' name='subcategory'>
                                                     <option>Motor Claims</option>
                                                     <option>Non Motor Claims</option>
                                                     <option>Policy Serving</option>
@@ -79,42 +79,43 @@
                                             <label for="location-input" class="col-md-2 col-form-label">Location</label>
                                             <div class="col-md-7">
                                                 <input class="form-control" type="text" placeholder="location"
-                                                    id="location">
+                                                    id="location" name="location">
                                             </div>
                                         </div>
                                         <div class="mb-3 row">
                                             <label for="branch-input" class="col-md-2 col-form-label">Branch</label>
                                             <div class="col-md-7">
-                                                <input class="form-control" type="text" placeholder="Branch" id="branch">
+                                                <input class="form-control" type="text" placeholder="Branch" id="branch" name="branch">
                                             </div>
                                         </div>
                                         <div class="mb-3 row">
                                             <label for="resperson-input" class="col-md-2 col-form-label">Responsible Person</label>
                                             <div class="col-md-7">
                                                 <input class="form-control" type="text" value="" placeholder="Responsible Person's Name"
-                                                    id="resperson">
+                                                    id="resperson" name="resperson">
                                             </div>
                                         </div>
                                         <div class="mb-3 row">
                                             <label for="altperson-input" class="col-md-2 col-form-label">Alternative Person</label>
                                             <div class="col-md-7">
                                                 <input class="form-control" type="text" value="" placeholder="Alternative Person's Name"
-                                                    id="altperson">
+                                                    id="altperson" name="altperson">
                                             </div>
                                         </div>
                                         <div class="mb-3 row">
                                             <label for="pred-input" class="col-md-2 col-form-label">Predefined Text</label>
                                             <div class="col-md-7">
                                                 <input class="form-control" type="text" value="" placeholder=""
-                                                    id="pred">
+                                                    id="pred" name="pred">
                                             </div>
                                         </div>
                                         <div class="mb-4 row">
                                             <label for="description-textarea" class="col-md-2 col-form-label">Description of the complaint</label>
                                             <div class="col-md-7">
                                                 <input class="form-control" type="text" value="" placeholder="Enter the description of the complaint"
-                                                    id="description" rows="5">
+                                                    id="description" rows="5" name="description">
                                             </div>
+                                        </div>
                                         <!--submit button layout-->
                                         <div class='col-sm-6'>
                                             <div>
@@ -130,6 +131,7 @@
 
 
                     </div>
+                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 
 
