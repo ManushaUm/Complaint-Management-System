42ed6d9e19abce97fd2b99bc1acee3e96674e6e2
G.C.M. <charithmanujaya1@gmail.com>
2025-03-26 20:46:29 +0530

    Create Kernel.php

---
+++ app/Console/Kernel.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Console;
+
+use Illuminate\Console\Scheduling\Schedule;
+use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
+use Illuminate\Support\Facades\DB;
+
+class Kernel extends ConsoleKernel
+{
+    /**
+     * Define the application's command schedule.
+     */
+    protected function schedule(Schedule $schedule)
+    {
+        // Schedule a task to delete expired memos every minute
+        $schedule->call(function () {
+            DB::table('memos')->where('timer', '<', now())->delete();
+        })->everyMinute();
+    }
+
+    /**
+     * Register the commands for the application.
+     */
+    protected function commands()
+    {
+        $this->load(__DIR__ . '/Commands');
+
+        require base_path('routes/console.php');
+    }
+}
